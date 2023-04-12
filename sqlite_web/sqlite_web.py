71ca462490bf64e1a3c6c8afd4e5f7d35d8ed091
spacewalkingninja <spacewalkingninja@gmail.com>
2023-04-12 16:22:37 +0100

    Update sqlite_web.py

--- sqlite_web/sqlite_web.py
+++ sqlite_web/sqlite_web.py
@@ -518,8 +518,8 @@ def table_content(table):
 @require_table
 def edit_row(table, row_id):
     ds_table = dataset[table]
-    primary_key = ds_table.get_primary_key(table)  # get the primary key column name
-    print(primary_key)
+    all_indexes = ds_table.get_indexes(table)  # get the primary key column name
+    print(all_indexes)
     row = ds_table.get(id=row_id)
     if request.method == 'POST':
         for field_name in ds_table.columns:
